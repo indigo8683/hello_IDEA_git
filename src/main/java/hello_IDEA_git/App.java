dccf3b229bf3c5fe64c7549317d09eda1b427514
indigo8683 <513648683@qq.com>
2022-03-16 17:20:29 +0800

    test

--- src/main/java/hello_IDEA_git/App.java
+++ src/main/java/hello_IDEA_git/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World IDEA Git v1.4!" );
+        System.out.println( "Hello World IDEA Git v1.5!" );
     }
 }
